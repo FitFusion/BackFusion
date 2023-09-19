cfb84258f1e960f7991f37f2a92d4cbe4782624d
Z-100 <marvin131t@gmail.com>
2023-09-19 15:12:08 +0200

    Initial Commit 4

--- app/src/main/kotlin/ch/fitfusion/backfusion/app/BackFusionApplication.kt
+++ app/src/main/kotlin/ch/fitfusion/backfusion/app/BackFusionApplication.kt
@@ -1,9 +1,17 @@
 package ch.fitfusion.backfusion.app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
+@ComponentScan(
+    "ch.fitfusion.backfusion",
+)
+@EntityScan(
+    "ch.fitfusion.backfusion",
+)
 @SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 open class BackFusionApplication
 
---
+++ backfusion-rs-api/src/main/kotlin/ch/fitfusion/backfusion/rs/api/TestResourceService.kt
@@ -0,0 +1,18 @@
+package ch.fitfusion.backfusion.rs.api
+
+import ch.fitfusion.backfusion.api.dtos.TestDTO
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.ResponseEntity
+
+@Tag(name = "Test Services")
+interface TestResourceService {
+
+    @Operation(summary = "Get a TestDTO from the API!")
+    @ApiResponse(
+        responseCode = "200",
+        description = "TestDTO",
+    )
+    fun test(): ResponseEntity<TestDTO>
+}
