78d3a7e835b3c2ca0abeaa06d541981205f47c02
Z-100 <marvin131t@gmail.com>
2023-10-13 17:17:45 +0200

    feature(account): Add basic auth

--- app/src/main/kotlin/ch/fitfusion/backfusion/app/filters/JWTAuthenticationFilter.kt
+++ app/src/main/kotlin/ch/fitfusion/backfusion/app/filters/JWTAuthenticationFilter.kt
@@ -6,10 +6,13 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken.unauthenticated
 import org.springframework.security.core.Authentication
-import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.stereotype.Component
 import org.springframework.web.HttpRequestMethodNotSupportedException
+import java.nio.charset.StandardCharsets.UTF_8
+import java.util.Base64
 
+@Component
 class JWTAuthenticationFilter : UsernamePasswordAuthenticationFilter() {
 
     private var postOnly = true
@@ -20,10 +23,9 @@ class JWTAuthenticationFilter : UsernamePasswordAuthenticationFilter() {
             throw HttpRequestMethodNotSupportedException("Method ${request.method} not supported.")
         }
 
-        val username = obtainUsername(request)?.trim { it <= ' ' } ?: ""
-        val password = obtainPassword(request) ?: ""
+        val basicAuthorization = getBasicAuthorization(request)
 
-        val authRequest = unauthenticated(username, password)
+        val authRequest = unauthenticated(basicAuthorization.username, basicAuthorization.password)
         setDetails(request, authRequest)
 
         return authenticationManager.authenticate(authRequest)
@@ -35,14 +37,20 @@ class JWTAuthenticationFilter : UsernamePasswordAuthenticationFilter() {
         chain: FilterChain?,
         authResult: Authentication?
     ) {
-
+        super.successfulAuthentication(request, response, chain, authResult)
     }
 
-    override fun unsuccessfulAuthentication(
-        request: HttpServletRequest?,
-        response: HttpServletResponse?,
-        failed: AuthenticationException?
-    ) {
-        super.unsuccessfulAuthentication(request, response, failed)
+    private fun getBasicAuthorization(request: HttpServletRequest?): BasicAuthorization {
+
+        val header = request?.getHeader("Authorization")?.substring("Basic ".length) ?: ""
+        val base64Credentials = Base64.getDecoder().decode(header)
+        val credentials = String(base64Credentials, UTF_8).split(":")
+
+        return BasicAuthorization(credentials[0], credentials[1])
     }
+
+    private data class BasicAuthorization(
+        val username: String,
+        val password: String,
+    )
 }
