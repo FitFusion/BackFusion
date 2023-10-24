25c4976bd73f5f7dd1daa0b80e2a354a08c7a78d
Z-100 <marvin131t@gmail.com>
2023-10-24 12:03:18 +0200

    feature(db-setup): Add mappers

--- backfusion/src/main/kotlin/ch/fitfusion/backfusion/workout/mappers/ExerciseMapper.kt
+++ backfusion/src/main/kotlin/ch/fitfusion/backfusion/workout/mappers/ExerciseMapper.kt
@@ -2,7 +2,9 @@ package ch.fitfusion.backfusion.workout.mappers
 
 import ch.fitfusion.backfusion.api.workout.dtos.ExerciseDTO
 import ch.fitfusion.backfusion.workout.entities.Exercise
+import ch.fitfusion.backfusion.workout.entities.Workout
 import org.mapstruct.Mapper
+import org.springframework.stereotype.Component
 
 @Mapper(componentModel = "spring")
 interface ExerciseMapper {
@@ -10,4 +12,30 @@ interface ExerciseMapper {
     fun toDTO(entity: Exercise): ExerciseDTO
 
     fun toEntity(dto: ExerciseDTO): Exercise
+
+    fun setParentEntity(child: Exercise, parent: Workout) {
+        child.workout = parent
+    }
+}
+
+@Component
+class ExerciseMapperImpl : ExerciseMapper {
+
+    override fun toDTO(entity: Exercise): ExerciseDTO {
+        return ExerciseDTO(
+            entity.id ?: -1,
+            entity.name,
+            entity.duration,
+        )
+    }
+
+    override fun toEntity(dto: ExerciseDTO): Exercise {
+        val entity = Exercise()
+
+        entity.id = dto.id
+        entity.name = dto.name
+        entity.duration = dto.duration
+
+        return entity
+    }
 }
